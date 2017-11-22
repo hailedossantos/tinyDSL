830774ca50d08be0530089836fe395122300f71e
hds <haile.dossantos@gmail.com>
2017-11-23 00:56:50 +0100

    syntax check

--- tinyDSL.py
+++ tinyDSL.py
@@ -1,9 +1,9 @@
 import sys
 import json
 
-sys.argv
+# sys.argv
 
-data = json.loads(argv[0])
+# data = json.loads(argv[0])
 
 class TinyDSL:
     """Tiny DSL class"""
@@ -23,12 +23,12 @@ class TinyDSL:
 
     def getSQL(json_query):
         result = "SELECT"
-        result.append(", ".join(json_query.get("fields")))
-        result.append("\t")
-        result.append("FROM crawls\t")
+        result += ", ".join(json_query.get("fields"))
+        result += "\t"
+        result += "FROM crawls\t"
         if json_query.has_key("filters"):
-            result.append("WHERE ")
-            result.append(getFilters(json_query.get("filters")))
+            result += "WHERE "
+            result += getFilters(json_query.get("filters"))
 
     def getFilters(json_filter):
         if len(json_filter) == 1:
@@ -38,3 +38,35 @@ class TinyDSL:
             return json_filter.get("field")+" "+predicates.get(json_filter.get("predicate"))+" "+json_filter.get("value")
 
     def checkSyntax(json_query):
+        if len(json_query) > 2:
+            return False
+        if not json_query.has_key("fields"):
+            return False
+        if not json_query.get("fields"):
+            return False
+        if json_query.has_key("filters"):
+            return checkFiltersSyntax(json_query.get("filters"))
+
+        return True
+
+    def checkFiltersSyntax(filters):
+        if filters.has_key("field"):
+            if not filters.has_key("value"):
+                return False
+            if len(filters) == 3:
+                if not filters.has_key("predicate"):
+                    return False
+                if not predicates.has_key(filters.get("predicate")):
+                    return False
+            return True
+        elif len(filters) == 1:
+            [(operator, child_filters)] = filters.items()
+            if not operator in logic_operators:
+                return False
+            if not len(child_filters) == 2:
+                return False
+            if not (checkFiltersSyntax(child_filters[0]) and checkFiltersSyntax(child_filters[1])):
+                return False
+            return True
+        else:
+            return False
